abededd8530a9b05066188e4f9a076d1eea64ec9
Wilmer <killerwilmer@gmail.com>
2017-03-27 23:04:29 -0500

    rendering a list of components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -11,11 +11,15 @@ class AlbumList extends Component {
     //debugger;
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
     console.log(this.state);
     return (
       <View>
-        <Text>Album List !!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
